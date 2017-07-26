cd648fbe75a37b25839474238d00498d0ff40b4e
Koji Wakamiya <koji.wakamiya@gmail.com>
2017-07-26 23:02:01 +0900

    Set up DataBinding

--- app/src/main/java/com/app/dr1009/githubsearch/MainActivity.kt
+++ app/src/main/java/com/app/dr1009/githubsearch/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.app.dr1009.githubsearch
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.app.dr1009.githubsearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
     }
 }
